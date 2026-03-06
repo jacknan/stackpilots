4a860672b27052ba915f39f7eee4090062e34959
mac <nan8278@126.com>
2026-03-06 22:15:58 +0800

    fix CSP for AdSense and Cloudflare Insights scripts

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cloud.umami.is pagead2.googlesyndication.com *.googlesyndication.com *.doubleclick.net *.googleadservices.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cloud.umami.is static.cloudflareinsights.com pagead2.googlesyndication.com *.googlesyndication.com *.doubleclick.net *.googleadservices.com ep1.adtrafficquality.google ep2.adtrafficquality.google;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
